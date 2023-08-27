0bba8ec8042f1eb39e790117dfccd7954e19fd14
BositG-7 <122206210+BositG-7@users.noreply.github.com>
2023-08-27 10:37:06 -0700

    added birbolola

--- src/pages/application/main-page/shopreducer.tsx
+++ src/pages/application/main-page/shopreducer.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-use-before-define */
+// shopReducer.ts
+
 import initialState from "./data";
 
 const shopReducer = (state = initialState, action: any) => {
@@ -8,9 +11,52 @@ const shopReducer = (state = initialState, action: any) => {
             korzine: [...state.korzine, action.payload]
          };
 
+      case "INCREMENT":
+         return incrementItem(state, action.payload);
+
+      case "DECREMENT":
+         return decrementItem(state, action.payload);
+
+      case "DELETE":
+         return deleteItem(state, action.payload);
+
       default:
          return state;
    }
+};
+
+const incrementItem = (state: any, productName: string) => {
+   console.log(productName);
+
+   const updatedKorzine = state.korzine.map((item: any) => {
+      if (item.name === productName) {
+         return { ...item, count: item.count + 1 };
+      }
+      return item;
+   });
+
+   return { ...state, korzine: updatedKorzine };
+};
+
+const decrementItem = (state: any, productName: string) => {
+   console.log(state);
+
+   console.log(productName);
+
+   const updatedKorzine = state.korzine.map((item: any) => {
+      if (item.name === productName && item.count > 0) {
+         return { ...item, count: item.count - 1 };
+      }
+      return item;
+   });
+
+   return { ...state, korzine: updatedKorzine };
+};
+
+const deleteItem = (state: any, productName: string) => {
+   const updatedKorzine = state.korzine.filter((item: any) => item.name !== productName);
+
+   return { ...state, korzine: updatedKorzine };
 };
 
 export default shopReducer;
